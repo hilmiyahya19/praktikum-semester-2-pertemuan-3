7b636a6904a579f2e61918a9b59a7646c1e87c60
hilmiyahya19 <91936796+hilmiyahya19@users.noreply.github.com>
2022-03-18 10:15:40 +0700

    Add files via upload

---
+++ libfunction.php
@@ -0,0 +1,57 @@
+<?php
+function kelulusan($rata_rata) {
+    if ($rata_rata <= 55){
+        return "Maaf anda tidak lulus!";
+    }else{
+        return "Selamat anda lulus!";
+    }
+}
+
+function grade($rata_rata) {
+    if ($rata_rata >= 85){
+        return "A";
+    }
+    elseif ($rata_rata >= 70){
+        return "B";
+    }
+    elseif ($rata_rata >= 56){
+        return "C";
+    }
+    elseif ($rata_rata >= 36){
+        return "D";
+    }
+    elseif ($rata_rata >= 10){
+        return "E";
+    }else{
+        return "Tidak ada nilai";
+    } 
+}
+
+function predikat($grade) {
+    switch ($grade) {
+        case "A" : 
+            return "Sangat memuaskan!";
+            break;
+        
+        case "B" : 
+            return "Memuaskan!";
+            break;
+
+        case "C" : 
+            return "Cukup!";
+            break;
+
+        case "D" : 
+            return "Kurang!";
+            break;
+        
+        case "E" : 
+            return "Sangat kurang!";
+            break;
+
+        case "I" : 
+            return "Tidak ada!";
+            break;
+    }
+}
+?>
